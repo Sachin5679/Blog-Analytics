b0a25c68e8db6fa523c302c520e08e6a1388523d
Sachin Jayadev <sachinjayadev18@gmail.com>
2023-09-30 19:58:02 +0530

    created get route to fetch blog data

--- index.js
+++ index.js
@@ -1,5 +1,21 @@
 const express = require('express');
+const axios = require('axios');
+require('dotenv').config();
+
 const app = express();
 const port = process.env.PORT || 5000;
+
+app.get('/api/blog-stats', async(req, res) => {
+    try{
+        const response = await axios.get('https://intent-kit-16.hasura.app/api/rest/blogs', {
+            headers: {
+                'x-hasura-admin-secret': process.env.ADMIN_SECRET
+            }
+        });
+        res.json(response.data)
+    } catch(err) {
+        res.status(500).json({error: "Error"});
+    }
+})
 
 app.listen(port, () => console.log(`Listening on localhost:${port}`))
